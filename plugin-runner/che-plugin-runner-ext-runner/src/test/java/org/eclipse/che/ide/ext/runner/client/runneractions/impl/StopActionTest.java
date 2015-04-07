0d89b19a9a009e636e65333eed252a1458cdc16a
Dmitry Shnurenko <dshnurenko@codenvy.com>
2015-04-07 14:22:21 +0300

    Fixed runner module build

--- plugin-runner/che-plugin-runner-ext-runner/src/test/java/org/eclipse/che/ide/ext/runner/client/runneractions/impl/StopActionTest.java
+++ plugin-runner/che-plugin-runner-ext-runner/src/test/java/org/eclipse/che/ide/ext/runner/client/runneractions/impl/StopActionTest.java
@@ -51,6 +51,7 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
