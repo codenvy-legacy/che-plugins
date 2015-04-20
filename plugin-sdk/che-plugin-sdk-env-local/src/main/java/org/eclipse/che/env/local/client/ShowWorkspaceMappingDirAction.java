4a5aa386c78d70a5f670d679b8760d39d9029ee4
Vitaly Parfonov <vparfonov@codenvy.com>
2015-04-20 18:46:48 +0300

    IDEX-2057
    Conflicts:
    	plugin-sdk/che-plugin-sdk-env-local/src/main/java/org/eclipse/che/env/local/client/WorkspaceMappingPresenter.java
    	plugin-sdk/che-plugin-sdk-env-local/src/main/java/org/eclipse/che/env/local/client/inject/LocalEnvironmentGinModule.java
    	plugin-sdk/che-plugin-sdk-ext-plugins/src/main/java/org/eclipse/che/env/local/client/WorkspaceToDirectoryMappingServiceClient.java

---
+++ plugin-sdk/che-plugin-sdk-env-local/src/main/java/org/eclipse/che/env/local/client/ShowWorkspaceMappingDirAction.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.env.local.client;
+
+import com.google.inject.Inject;
+
+import org.eclipse.che.ide.api.action.Action;
+import org.eclipse.che.ide.api.action.ActionEvent;
+
+/**
+ * Action for showing About application information.
+ *
+ * @author Ann Shumilova
+ */
+public class ShowWorkspaceMappingDirAction extends Action {
+
+    private final WorkspaceMappingPresenter presenter;
+
+
+    @Inject
+    public ShowWorkspaceMappingDirAction(WorkspaceMappingPresenter presenter, LocalizationConstant locale) {
+        super(locale.actionWorkspaceLocation(), locale.actionWorkspaceLocationTitle());
+        this.presenter = presenter;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        presenter.showDialog();
+    }
+
+}
