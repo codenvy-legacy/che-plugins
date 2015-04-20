9c06579ac4120c1a65c496ef6a8fd64d3a47ef51
Vitaly Parfonov <vparfonov@codenvy.com>
2015-04-20 19:58:43 +0300

    Merge with master

---
+++ plugin-sdk/che-plugin-sdk-ext-plugins/src/main/java/org/eclipse/che/env/local/client/SdkDocumentTitleDecorator.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.env.local.client;
+
+import com.google.inject.Inject;
+
+import org.eclipse.che.ide.api.DocumentTitleDecorator;
+
+/**
+ * @author Vitaly Parfonov
+ */
+public class SdkDocumentTitleDecorator implements DocumentTitleDecorator {
+
+    private LocalizationConstant localizationConstant;
+
+    @Inject
+    public SdkDocumentTitleDecorator(LocalizationConstant localizationConstant) {
+
+        this.localizationConstant = localizationConstant;
+    }
+
+    @Override
+    public String getDocumentTitle() {
+        return localizationConstant.cheTabTitle();
+    }
+
+    @Override
+    public String getDocumentTitle(String project) {
+        return localizationConstant.cheTabTitle(project);
+    }
+
+}
---
+++ plugin-sdk/che-plugin-sdk-ext-plugins/src/main/java/org/eclipse/che/env/local/client/inject/LocalEnvironmentGinModule.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.env.local.client.inject;
+
+import com.google.gwt.inject.client.AbstractGinModule;
+import com.google.gwt.inject.client.multibindings.GinMultibinder;
+import com.google.inject.Singleton;
+
+import org.eclipse.che.env.local.client.ActionDenyAccessDialogLocalEnv;
+import org.eclipse.che.env.local.client.ResourcesLockedActionPermitLocalEnv;
+import org.eclipse.che.env.local.client.SdkDocumentTitleDecorator;
+import org.eclipse.che.env.local.client.WorkspaceMappingPresenter;
+import org.eclipse.che.env.local.client.WorkspaceToDirectoryMappingServiceClient;
+import org.eclipse.che.env.local.client.WorkspaceToDirectoryMappingServiceClientImpl;
+import org.eclipse.che.ide.api.DocumentTitleDecorator;
+import org.eclipse.che.ide.api.action.permits.ActionDenyAccessDialog;
+import org.eclipse.che.ide.api.action.permits.ResourcesLockedActionPermit;
+import org.eclipse.che.ide.api.action.permits.Build;
+import org.eclipse.che.ide.api.action.permits.Run;
+import org.eclipse.che.ide.api.extension.ExtensionGinModule;
+import org.eclipse.che.ide.api.preferences.PreferencePagePresenter;
+
+/**
+ * @author Vitaly Parfonov
+ */
+@ExtensionGinModule
+public class LocalEnvironmentGinModule extends AbstractGinModule {
+    @Override
+    protected void configure() {
+        bind(ResourcesLockedActionPermit.class).annotatedWith(Build.class).to(ResourcesLockedActionPermitLocalEnv.class).in(Singleton.class);
+        bind(ActionDenyAccessDialog.class).annotatedWith(Build.class).to(ActionDenyAccessDialogLocalEnv.class).in(Singleton.class);
+        bind(ResourcesLockedActionPermit.class).annotatedWith(Run.class).to(ResourcesLockedActionPermitLocalEnv.class).in(Singleton.class);
+        bind(ActionDenyAccessDialog.class).annotatedWith(Run.class).to(ActionDenyAccessDialogLocalEnv.class).in(Singleton.class);
+        bind(ResourcesLockedActionPermit.class).to(ResourcesLockedActionPermitLocalEnv.class).in(Singleton.class);
+        bind(DocumentTitleDecorator.class).to(SdkDocumentTitleDecorator.class).in(Singleton.class);
+        bind(WorkspaceToDirectoryMappingServiceClient.class).to(WorkspaceToDirectoryMappingServiceClientImpl.class).in(Singleton.class);
+
+        bind(WorkspaceMappingPresenter.class).in(Singleton.class);
+    }
+}
