11e1d9d0422f505c20bcda86e7d5206dcb73652a
Vitaly Parfonov <vparfonov@codenvy.com>
2015-04-20 23:47:51 +0300

    code clean up

--- plugin-sdk/che-plugin-sdk-env-local/src/main/java/org/eclipse/che/env/local/client/WorkspaceMappingPresenter.java
+++ plugin-sdk/che-plugin-sdk-env-local/src/main/java/org/eclipse/che/env/local/client/WorkspaceMappingPresenter.java
@@ -14,7 +14,6 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.web.bindery.event.shared.EventBus;
 
-import org.eclipse.che.ide.api.DocumentTitleDecorator;
 import org.eclipse.che.ide.api.app.AppContext;
 import org.eclipse.che.ide.api.event.RefreshProjectTreeEvent;
 import org.eclipse.che.ide.api.notification.NotificationManager;
