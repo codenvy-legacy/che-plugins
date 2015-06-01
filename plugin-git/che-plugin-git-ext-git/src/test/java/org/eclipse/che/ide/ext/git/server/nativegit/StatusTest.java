e23a6c7ff2ee5a4d23355e7a7b2adb67c5f1e2e4
Tareq Sharafy <tareq.sharafy@sap.com>
2015-06-01 10:16:22 +0300

    Align the native git status lists with standard git
    Signed-off-by: Tareq Sharafy <tareq.sharafy@sap.com>

--- plugin-git/che-plugin-git-ext-git/src/test/java/org/eclipse/che/ide/ext/git/server/nativegit/StatusTest.java
+++ plugin-git/che-plugin-git-ext-git/src/test/java/org/eclipse/che/ide/ext/git/server/nativegit/StatusTest.java
@@ -185,7 +185,7 @@ public class StatusTest extends BaseTest {
         final Status status = getConnection().status(SHORT);
 
         assertEquals(status.getMissing(), asList("a"));
-        assertTrue(status.getAdded().isEmpty());
+        assertEquals(status.getAdded(), asList("a"));
         assertTrue(status.getChanged().isEmpty());
         assertTrue(status.getConflicting().isEmpty());
         assertTrue(status.getRemoved().isEmpty());
@@ -206,11 +206,11 @@ public class StatusTest extends BaseTest {
 
         final Status status = getConnection().status(SHORT);
 
-        assertEquals(status.getRemoved(), asList("a"));
+        assertTrue(status.getRemoved().isEmpty());
         assertTrue(status.getAdded().isEmpty());
         assertTrue(status.getChanged().isEmpty());
         assertTrue(status.getConflicting().isEmpty());
-        assertTrue(status.getMissing().isEmpty());
+        assertEquals(status.getMissing(), asList("a"));
         assertTrue(status.getUntracked().isEmpty());
         assertTrue(status.getUntrackedFolders().isEmpty());
     }
