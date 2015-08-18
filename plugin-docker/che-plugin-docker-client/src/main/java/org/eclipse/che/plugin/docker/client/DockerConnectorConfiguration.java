0f74982164afc93aab6032936873d0b421b7a33f
Florent BENOIT <fbenoit@codenvy.com>
2015-08-18 14:21:10 +0200

    IDEX-2905 Allow to use Docker Machine (Toolbox) system variables Note: boot2docker and Docker Machine are using the same system variables

--- plugin-docker/che-plugin-docker-client/src/main/java/org/eclipse/che/plugin/docker/client/DockerConnectorConfiguration.java
+++ plugin-docker/che-plugin-docker-client/src/main/java/org/eclipse/che/plugin/docker/client/DockerConnectorConfiguration.java
@@ -14,21 +14,40 @@ import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
 import org.eclipse.che.api.core.util.SystemInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nonnull;
 import java.io.File;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+
+import static org.eclipse.che.plugin.docker.client.DockerConnector.DEFAULT_DOCKER_MACHINE_CERTS_DIR;
+import static org.eclipse.che.plugin.docker.client.DockerConnector.DEFAULT_DOCKER_MACHINE_URI;
+import static org.eclipse.che.plugin.docker.client.DockerConnector.DOCKER_CERT_PATH_PROPERTY;
+import static org.eclipse.che.plugin.docker.client.DockerConnector.DOCKER_HOST_PROPERTY;
+import static org.eclipse.che.plugin.docker.client.DockerConnector.DOCKER_TLS_VERIFY_PROPERTY;
+import static org.eclipse.che.plugin.docker.client.DockerConnector.UNIX_SOCKET_URI;
 
 /**
  * @author Alexander Garagatyi
+ * @author Florent Benoit
  */
 public class DockerConnectorConfiguration {
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(DockerConnectorConfiguration.class);
+
     @Inject(optional = true)
     @Named("docker.client.daemon_url")
     private URI dockerDaemonUri = dockerDaemonUri();
 
     @Inject(optional = true)
     @Named("docker.client.certificates_folder")
-    private String dockerCertificatesDirectoryPath = boot2dockerCertsDirectoryPath();
+    private String dockerCertificatesDirectoryPath = dockerMachineCertsDirectoryPath();
 
     @Inject
     private InitialAuthConfig authConfigs;
@@ -47,11 +66,87 @@ public class DockerConnectorConfiguration {
     }
 
     private static URI dockerDaemonUri() {
-        return SystemInfo.isLinux() ? DockerConnector.UNIX_SOCKET_URI : DockerConnector.BOOT2DOCKER_URI;
+        return dockerDaemonUri(SystemInfo.isLinux(), System.getenv());
+
     }
 
-    private static String boot2dockerCertsDirectoryPath() {
-        return SystemInfo.isLinux() ? null : DockerConnector.BOOT2DOCKER_CERTS_DIR;
+    /**
+     * Provides the URI that will be used to connect on docker.
+     * It may use extra environment to help to build this URI
+     *
+     * @param isLinux
+     *         if System is running on Linux
+     * @param env
+     *         should contain System environment
+     * @return URI to connect to docker
+     */
+    protected static URI dockerDaemonUri(final boolean isLinux, @Nonnull final Map<String, String> env) {
+        if (isLinux) {
+            return UNIX_SOCKET_URI;
+        }
+
+        // check if have docker variables
+        String host = env.get(DOCKER_HOST_PROPERTY);
+        if (host != null) {
+            // user has defined some properties, use them
+            URI userURI;
+            try {
+                userURI = new URI(host);
+            } catch (URISyntaxException e) {
+                LOG.error(String.format("Unable to parse %s property with value %s", DOCKER_HOST_PROPERTY, host), e);
+                // unable to use given property, fallback to default URL
+                return DEFAULT_DOCKER_MACHINE_URI;
+            }
+
+            // Secure connection ?
+            String tls = env.get(DOCKER_TLS_VERIFY_PROPERTY);
+            String protocol = "1".equals(tls) ? "https" : "http";
+
+            // build URI
+            try {
+                return new URI(protocol, null, userURI.getHost(), userURI.getPort(), null, null, null);
+            } catch (URISyntaxException e) {
+                LOG.error(String.format("Unable to create URI %s property with value %s and TLS %s", DOCKER_HOST_PROPERTY, host,
+                                        DOCKER_TLS_VERIFY_PROPERTY), e);
+                // unable to use given property, fallback to default URL
+                return DEFAULT_DOCKER_MACHINE_URI;
+
+            }
+        }
+        return DEFAULT_DOCKER_MACHINE_URI;
+    }
+
+    private static String dockerMachineCertsDirectoryPath() {
+        return dockerMachineCertsDirectoryPath(SystemInfo.isLinux(), System.getenv());
+    }
+
+    /**
+     * Provides the location of certificates used to connect on docker.
+     * It may use extra environment to help to build this path
+     *
+     * @param isLinux
+     *         if System is running on Linux
+     * @param env
+     *         should contain System environment
+     * @return local path of the docker certificates
+     */
+    protected static String dockerMachineCertsDirectoryPath(boolean isLinux, @Nonnull Map<String, String> env) {
+        if (isLinux) {
+            return null;
+        }
+        // check if have boot2docker variables
+        String certPath = env.get(DOCKER_CERT_PATH_PROPERTY);
+
+        // return it only if it exists
+        if (certPath != null) {
+            if (new File(certPath).exists()) {
+                return certPath;
+            } else {
+                LOG.error(String.format("The directory provided by property %s doesn't exists. Returning default value", certPath));
+            }
+        }
+        // return default value
+        return DEFAULT_DOCKER_MACHINE_CERTS_DIR;
     }
 
     public URI getDockerDaemonUri() {
