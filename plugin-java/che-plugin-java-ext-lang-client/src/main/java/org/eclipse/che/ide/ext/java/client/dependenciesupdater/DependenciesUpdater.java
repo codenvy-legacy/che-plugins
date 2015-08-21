fe2dcd2ae98a3659e0b627689788f9322cf86497
Artem Zatsarynnyy <azatsarynnyy@codenvy.com>
2015-08-21 16:51:59 +0300

    IDEX-2874: add method for getting project's classpath

--- plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/dependenciesupdater/DependenciesUpdater.java
+++ plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/dependenciesupdater/DependenciesUpdater.java
@@ -12,27 +12,18 @@ package org.eclipse.che.ide.ext.java.client.dependenciesupdater;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import com.google.web.bindery.event.shared.EventBus;
 
 import org.eclipse.che.api.project.shared.dto.ProjectDescriptor;
 import org.eclipse.che.ide.api.app.AppContext;
 import org.eclipse.che.ide.api.app.CurrentProject;
-import org.eclipse.che.ide.api.editor.EditorAgent;
-import org.eclipse.che.ide.api.editor.EditorPartPresenter;
-import org.eclipse.che.ide.api.event.RefreshProjectTreeEvent;
 import org.eclipse.che.ide.api.notification.Notification;
 import org.eclipse.che.ide.api.notification.NotificationManager;
 import org.eclipse.che.ide.ext.java.client.JavaLocalizationConstant;
-import org.eclipse.che.ide.ext.java.client.projecttree.JavaTreeStructure;
-import org.eclipse.che.ide.ext.java.client.projecttree.nodes.ExternalLibrariesNode;
-import org.eclipse.che.ide.jseditor.client.texteditor.EmbeddedTextEditorPresenter;
 import org.eclipse.che.ide.rest.AsyncRequestCallback;
-import org.eclipse.che.ide.rest.DtoUnmarshallerFactory;
 import org.eclipse.che.ide.util.Pair;
 import org.eclipse.che.ide.util.loging.Log;
 
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
 
 import static org.eclipse.che.ide.api.notification.Notification.Status.FINISHED;
@@ -47,40 +38,24 @@ import static org.eclipse.che.ide.api.notification.Notification.Type.ERROR;
  */
 @Singleton
 public class DependenciesUpdater {
-//    private final BuilderConsolePresenter          builderConsole;
-    private final NotificationManager              notificationManager;
-    private final EditorAgent                      editorAgent;
-//    private final BuildController                  buildController;
-    private final DtoUnmarshallerFactory           dtoUnmarshallerFactory;
-    private final EventBus                         eventBus;
-    private final AppContext                       context;
-    private final JavaNameEnvironmentServiceClient nameEnvironmentServiceClient;
-    private final JavaLocalizationConstant         javaLocalizationConstant;
+    private final NotificationManager        notificationManager;
+    private final AppContext                 context;
+    private final JavaClasspathServiceClient classpathServiceClient;
+    private final JavaLocalizationConstant   javaLocalizationConstant;
 
     private Queue<Pair<ProjectDescriptor, Boolean>> projects = new LinkedList<>();
     private boolean                                 updating = false;
-    private JavaTreeStructure javaTreeStructure;
-    private Notification      notification;
+    private Notification notification;
 
     @Inject
     public DependenciesUpdater(JavaLocalizationConstant javaLocalizationConstant,
                                NotificationManager notificationManager,
-                               EditorAgent editorAgent,
-//                               BuildController buildController,
-                               DtoUnmarshallerFactory dtoUnmarshallerFactory,
-                               EventBus eventBus,
                                AppContext context,
-                               JavaNameEnvironmentServiceClient nameEnvironmentServiceClient
-                               /*BuilderConsolePresenter builderConsole*/) {
+                               JavaClasspathServiceClient classpathServiceClient) {
         this.javaLocalizationConstant = javaLocalizationConstant;
         this.notificationManager = notificationManager;
-        this.editorAgent = editorAgent;
-//        this.buildController = buildController;
-        this.dtoUnmarshallerFactory = dtoUnmarshallerFactory;
-        this.eventBus = eventBus;
         this.context = context;
-        this.nameEnvironmentServiceClient = nameEnvironmentServiceClient;
-//        this.builderConsole = builderConsole;
+        this.classpathServiceClient = classpathServiceClient;
     }
 
     public void updateDependencies(final ProjectDescriptor project, final boolean force) {
@@ -94,34 +69,15 @@ public class DependenciesUpdater {
             return;
         }
 
-//        builderConsole.clear();
-
-        javaTreeStructure = null;
-        if (currentProject.getCurrentTree() instanceof JavaTreeStructure) {
-            javaTreeStructure = (JavaTreeStructure)currentProject.getCurrentTree();
-        }
         notification = new Notification(javaLocalizationConstant.updatingDependencies(), PROGRESS);
         notificationManager.showNotification(notification);
-
-//        buildContext.setBuilding(true);
         updating = true;
 
-        // send a first request to launch build process and return build task descriptor
-//        final Unmarshallable<BuildTaskDescriptor> unmarshaller = dtoUnmarshallerFactory.newUnmarshaller(BuildTaskDescriptor.class);
-        nameEnvironmentServiceClient.updateDependencies(
+        classpathServiceClient.updateDependencies(
                 project.getPath(), force, new AsyncRequestCallback<Boolean>() {
                     @Override
                     protected void onSuccess(Boolean descriptor) {
-////                        buildContext.setBuildTaskDescriptor(descriptor);
-//
-//                        if (descriptor.getStatus() == BuildStatus.SUCCESSFUL) {
                         onUpdated();
-//                            return;
-//                        }
-//                        buildController.showRunningBuild(descriptor, "[INFO] Updating dependencies...");
-//
-//                        // send a second request to be notified when dependencies update is finished
-//                        updateAndWait(descriptor, project);
                     }
 
                     @Override
@@ -133,61 +89,13 @@ public class DependenciesUpdater {
                 });
     }
 
-//    private void updateAndWait(BuildTaskDescriptor descriptor, final ProjectDescriptor project) {
-//        nameEnvironmentServiceClient.updateDependenciesAndWait(project.getPath(), descriptor, new AsyncRequestCallback<Void>() {
-//            @Override
-//            protected void onSuccess(Void result) {
-//                onUpdated();
-//                parserWorker.dependenciesUpdated();
-//                refreshOpenedEditors();
-//                refreshExtLibs(project);
-//
-//                if (!projects.isEmpty()) {
-//                    Pair<ProjectDescriptor, Boolean> pair = projects.poll();
-//                    updateDependencies(pair.first, pair.second);
-//                }
-//            }
-//
-//            @Override
-//            protected void onFailure(Throwable exception) {
-//                updating = false;
-//                if (!projects.isEmpty()) {
-//                    Pair<ProjectDescriptor, Boolean> pair = projects.poll();
-//                    updateDependencies(pair.first, pair.second);
-//                }
-//                updateFinishedWithError(exception, notification);
-//            }
-//        });
-//    }
-
     private void onUpdated() {
         updating = false;
         notification.setMessage(javaLocalizationConstant.dependenciesSuccessfullyUpdated());
         notification.setStatus(FINISHED);
-//        buildContext.setBuilding(false);
-    }
-
-    private void refreshOpenedEditors() {
-        Map<String, EditorPartPresenter> openedEditors = editorAgent.getOpenedEditors();
-        for(EditorPartPresenter editorPartPresenter: openedEditors.values()) {
-            if (editorPartPresenter instanceof EmbeddedTextEditorPresenter) {
-                final EmbeddedTextEditorPresenter<?> editor = (EmbeddedTextEditorPresenter<?>)editorPartPresenter;
-                editor.refreshEditor();
-            }
-        }
-    }
-
-    private void refreshExtLibs(ProjectDescriptor project) {
-        if (javaTreeStructure != null) {
-            ExternalLibrariesNode librariesNode = javaTreeStructure.getExternalLibrariesNode(project.getPath());
-            if (librariesNode != null && librariesNode.isOpened()) {
-                eventBus.fireEvent(new RefreshProjectTreeEvent(librariesNode));
-            }
-        }
     }
 
     private void updateFinishedWithError(Throwable exception, Notification notification) {
-//        buildContext.setBuilding(false);
         notification.setMessage(exception.getMessage());
         notification.setType(ERROR);
         notification.setStatus(FINISHED);
--- plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/dependenciesupdater/JavaClasspathServiceClientImpl.java
+++ plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/dependenciesupdater/JavaClasspathServiceClientImpl.java
@@ -14,39 +14,46 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.inject.name.Named;
 
-import org.eclipse.che.ide.api.app.AppContext;
 import org.eclipse.che.ide.rest.AsyncRequestCallback;
 import org.eclipse.che.ide.rest.AsyncRequestFactory;
+
+import java.util.List;
 
 import static org.eclipse.che.ide.MimeType.APPLICATION_JSON;
 import static org.eclipse.che.ide.rest.HTTPHeader.ACCEPT;
 
 /**
- * Implementation of {@link JavaNameEnvironmentServiceClient}.
+ * Implementation of {@link JavaClasspathServiceClient}.
  *
  * @author Artem Zatsarynnyy
  */
 @Singleton
-public class JavaNameEnvironmentServiceClientImpl implements JavaNameEnvironmentServiceClient {
+public class JavaClasspathServiceClientImpl implements JavaClasspathServiceClient {
+
     private final AsyncRequestFactory asyncRequestFactory;
     private final String              extPath;
     private       String              workspaceId;
-    private final AppContext appContext;
 
     @Inject
-    protected JavaNameEnvironmentServiceClientImpl(AsyncRequestFactory asyncRequestFactory,
-                                                   @Named("cheExtensionPath") String extPath,
-                                                   @Named("workspaceId") String workspaceId,
-                                                   AppContext appContext) {
+    protected JavaClasspathServiceClientImpl(AsyncRequestFactory asyncRequestFactory,
+                                             @Named("cheExtensionPath") String extPath,
+                                             @Named("workspaceId") String workspaceId) {
         this.asyncRequestFactory = asyncRequestFactory;
         this.extPath = extPath;
         this.workspaceId = workspaceId;
-        this.appContext = appContext;
     }
 
     @Override
     public void updateDependencies(String projectPath, boolean force, AsyncRequestCallback<Boolean> callback) {
         final String requestUrl = extPath + "/jdt/" + workspaceId + "/classpath/update?projectpath=" + projectPath;
+        asyncRequestFactory.createGetRequest(requestUrl)
+                           .header(ACCEPT, APPLICATION_JSON)
+                           .send(callback);
+    }
+
+    @Override
+    public void getClasspath(String projectPath, AsyncRequestCallback<List<String>> callback) {
+        final String requestUrl = extPath + "/jdt/" + workspaceId + "/classpath?projectpath=" + projectPath;
         asyncRequestFactory.createGetRequest(requestUrl)
                            .header(ACCEPT, APPLICATION_JSON)
                            .send(callback);
