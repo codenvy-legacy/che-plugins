5aec9e818bbb872451253611d170a9b0f2b2f660
Artem Zatsarynnyy <azatsarynnyy@codenvy.com>
2015-08-27 17:56:21 +0300

    IDEX-2911: Adopt client side to new Workspace API

--- plugin-machine/che-plugin-machine-ext-client/src/test/java/org/eclipse/che/ide/extension/machine/client/MachineComponentTest.java
+++ plugin-machine/che-plugin-machine-ext-client/src/test/java/org/eclipse/che/ide/extension/machine/client/MachineComponentTest.java
@@ -65,20 +65,6 @@ public class MachineComponentTest {
     private ArgumentCaptor<Operation<List<MachineStateDescriptor>>> machinesCaptor;
 
     @Test
-    public void shouldStartNewDevMachine() throws Exception {
-        when(machineServiceClient.getMachinesStates(anyString())).thenReturn(machinesPromise);
-        when(machinesPromise.then(any(Operation.class))).thenReturn(machinesPromise);
-
-        machineComponent.start(componentCallback);
-
-        verify(machineServiceClient).getMachinesStates(anyString());
-        verify(machinesPromise).then(machinesCaptor.capture());
-        machinesCaptor.getValue().apply(Collections.singletonList(machineStateDescriptor));
-        verify(machineManager).startDevMachine(eq(MachineComponent.DEFAULT_RECIPE), anyString());
-        verify(componentCallback).onSuccess(eq(machineComponent));
-    }
-
-    @Test
     public void shouldUseRunningDevMachine() throws Exception {
         when(machineServiceClient.getMachinesStates(anyString())).thenReturn(machinesPromise);
         when(machinesPromise.then(any(Operation.class))).thenReturn(machinesPromise);
@@ -94,7 +80,7 @@ public class MachineComponentTest {
         verify(machineStateDescriptor).isDev();
         verify(machineStateDescriptor).getStatus();
         verify(appContext).setDevMachineId(eq(DEV_MACHINE_ID));
-        verify(machineManager, never()).startDevMachine(eq(MachineComponent.DEFAULT_RECIPE), anyString());
+        verify(machineManager, never()).startDevMachine(anyString(), anyString());
         verify(componentCallback).onSuccess(eq(machineComponent));
     }
 }
