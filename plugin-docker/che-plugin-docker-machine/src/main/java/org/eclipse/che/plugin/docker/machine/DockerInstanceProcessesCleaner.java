74a325a0759926af6fccd22465333167d63c9688
Alexander Garagatyi <agaragatyi@codenvy.com>
2015-12-22 12:41:56 +0200

    IDEX-3684: fix formatting of license

--- plugin-docker/che-plugin-docker-machine/src/main/java/org/eclipse/che/plugin/docker/machine/DockerInstanceProcessesCleaner.java
+++ plugin-docker/che-plugin-docker-machine/src/main/java/org/eclipse/che/plugin/docker/machine/DockerInstanceProcessesCleaner.java
@@ -1,8 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2012-2015 Codenvy, S.A. All rights reserved. This program and the accompanying materials are made available under the terms
- * of the Eclipse Public License v1.0 which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2012-2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors: Codenvy, S.A. - initial API and implementation
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
  *******************************************************************************/
 package org.eclipse.che.plugin.docker.machine;
 
